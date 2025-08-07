fa2027f89f7d16e66bf8150d2880b986595336e9
Austin Mthombeni <2586407@students.wits.ac.za>
2025-08-07 21:54:33 +0200

    redeployment

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -26,6 +26,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+       <h1>Hello World, We're building some weird event planning thing</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
