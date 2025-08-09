6078e1222a08e3bd4f4db44d1605b51062799a38
AustinMthombeni <2586407@students.wits.ac.za>
2025-08-09 20:54:27 +0200

    code coverage setup

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Signup from './pages/Signup.jsx';
 import Login from './pages/Login.jsx';
 import Home from './pages/Home.jsx';
-console.log(import.meta.env.VITE_API_KEY);
 
 import './App.css'
 
