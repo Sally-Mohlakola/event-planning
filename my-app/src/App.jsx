273dbee65a16bf599fb69fa4b8aa6541e502cb1d
Tawana7 <tawanakadango1@gmail.com>
2025-08-17 23:58:31 +0200

    Make Planner Dashbaord

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Signup from './pages/Signup.jsx';
 import Login from './pages/Login.jsx';
 import Home from './pages/Home.jsx';
+import PlannerDashboard from './pages/planner/PlannerDashboard.jsx';
 
 import './App.css'
 
@@ -14,7 +15,7 @@ function App() {
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/' element={<Signup />} />
+        <Route path='/' element={<PlannerDashboard />} />
       </Routes>
     </Router>
   )
