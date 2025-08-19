603114d9dd3e65afa87b4a791cdde53fb3ee7d9c
AustinMthombeni <2586407@students.wits.ac.za>
2025-08-19 13:53:31 +0200

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,9 @@ import Login from './pages/Login.jsx';
 import Home from './pages/Home.jsx';
 import Admin from './pages/adminDashboard/Admin.jsx';
 import PlannerDashboard from './pages/planner/PlannerDashboard.jsx';
+import PlannerManagement from './pages/adminDashboard/PlannerManagement.jsx;
+import EventManagement from './pages/adminDashboard/EventManagement.jsx;
+import VendorManagement from './pages/adminDashboard/VendorManagement.jsx;
 
 
 import './App.css'
@@ -18,6 +21,9 @@ function App() {
         
         <Route path='/signup' element={<Signup />} />
         <Route path='/' element={<Login />} />
+        <Route path='/planner-management' element ={<PlannerManagement/>} />\
+        <Route path='/event-management' element ={<EventManagement/>} />
+        <Route path='/vendor-management' element ={<VendorManagement/>} />
         <Route path='/login' element={<Login />} />
         <Route path='/home' element={<Home />} />
         <Route path='/admin' element={<Admin />} />
