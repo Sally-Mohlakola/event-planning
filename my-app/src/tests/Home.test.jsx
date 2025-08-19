3a431925b4745f57e6727567193cd1ae5f4b72c7
AustinMthombeni <2586407@students.wits.ac.za>
2025-08-19 14:37:32 +0200

    fix: codecov setup

--- my-app/src/tests/Home.test.jsx
+++ my-app/src/tests/Home.test.jsx
@@ -1,12 +1,17 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import { expect, test } from 'vitest'
+import { MemoryRouter } from 'react-router-dom' // <— import this
 import Home from '../pages/Home'
-import Login from '../pages/Login'
-
 
 test('renders sign-in success message', () => {
-  render(<Home />);
-  expect(screen.getByRole('heading', { name: /sign in works/i })).toBeInTheDocument();
-});
+  render(
+    <MemoryRouter> {/* <— wrap component in a router */}
+      <Home />
+    </MemoryRouter>
+  )
 
+  expect(
+    screen.getByRole('heading', { name: /sign in works/i })
+  ).toBeInTheDocument()
+})
