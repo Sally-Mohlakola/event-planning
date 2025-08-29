0d37c652d84cd25e900d3e440ab3406d39f567fc
Austin Mthombeni <2586407@students.wits.ac.za>
2025-08-29 22:19:38 +0200

    setup/firebase-functions

--- my-app/functions/index.js
+++ my-app/functions/index.js
@@ -6,4 +6,6 @@ const logger = require("firebase-functions/logger");
 exports.helloWorld = onRequest((request, response) => {
    logger.info("Hello logs!", {structuredData: true});
    response.send("Hello from Firebase!");
+
+   
  });
