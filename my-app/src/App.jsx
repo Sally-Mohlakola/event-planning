b0e6c6ec76a34718eda1c84ec6ee71707c25e7fd
Ngabelwe-01 <2586421@students.wits.ac.za>
2025-09-01 21:46:58 +0200

    Added LandingPage.jsx and LandingPage.css

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -13,6 +13,7 @@ import VendorProfileEdit from './pages/vendor/vendorProfileEdit.jsx';
 import VendorProfile from './pages/vendor/vendorProfile.jsx';
 import VendorApply from './pages/vendor/vendorApply.jsx';
 import Reports from './pages/adminDashboard/Reports.jsx';
+import LandingPage from './pages/LandingPage.jsx';
 
 
 
@@ -25,9 +26,10 @@ function App() {
     <Router>
       <Routes>
         
+        <Route path='/' element={<LandingPage />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/' element={<Login />} />
-        <Route path='/admin/planner-management' element ={<PlannerManagement/>} />\
+        <Route path='/login' element={<Login />} />
+        <Route path='/admin/planner-management' element ={<PlannerManagement/>} />
         <Route path='/admin/event-management' element ={<EventManagement/>} />
         <Route path='/admin/vendor-management' element ={<VendorManagement/>} />
         <Route path="/admin/reports" element={<Reports />} />
